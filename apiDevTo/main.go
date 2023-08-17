aa181c8fd2985b723c08bbde9ae84e729a7f039a
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-08-17 09:40:50 -0300

    Delimitando uma quantidade de publicações.

--- apiDevTo/main.go
+++ apiDevTo/main.go
@@ -1,39 +1,45 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Article struct {
-    Title       string `json:"title"`
-    Description string `json:"description"`
-    URL         string `json:"url"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
 }
 
 func main() {
-    username := "eumicaeu"
-    url := fmt.Sprintf("https://dev.to/api/articles?username=%s", username)
+	username := "costinmanda"
+	url := fmt.Sprintf("https://dev.to/api/articles?username=%s", username)
 
-    resp, err := http.Get(url)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 
-    var articles []Article
-    if err := json.Unmarshal(body, &articles); err != nil {
-        panic(err)
-    }
+	var articles []Article
+	if err := json.Unmarshal(body, &articles); err != nil {
+		panic(err)
+	}
+	cont := 0
 
-    for _, article := range articles {
-        fmt.Printf("%s: %s\n%s\n\n", article.Title, article.Description, article.URL)
-    }
+	for _, article := range articles {
+		if cont < 10 {
+			fmt.Printf("%s: %s\n%s\n\n", article.Title, article.Description, article.URL)
+			cont++
+		}
+	}
+
+	fmt.Print(cont)
 }
