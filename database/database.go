c13c4e5b956d7b27a6587dba5d325b8036901eda
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-09-09 19:29:38 -0300

    Finalizando a configração das envs.

--- database/database.go
+++ database/database.go
@@ -4,17 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
 func NewDB() (*sql.DB, error) {
 
-	dbUser := "DB_USER"
-	dbPassword := "DB_PASSWORD"
-	dbHost := "DB_HOST"
-	dbPort := "DB_PORT"
-	dbName := "DB_NAME"
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := sql.Open("postgres", dsn)
