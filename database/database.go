1a3316aca1f129a4675a1150c53d6d93eb61f515
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-09-13 23:00:11 -0300

    Organizando um pouco do CSS/HTML.

--- database/database.go
+++ database/database.go
@@ -4,18 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
+	//"os"
 
 	_ "github.com/lib/pq"
 )
 
 func NewDB() (*sql.DB, error) {
 
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	dbUser := "root"
+	dbPassword := "NsPP0Cl4Xus4NjgoUvLnkTFpUtz9nN5I"
+	dbHost := "dpg-cjd7jk7db61s73cdigjg-a.oregon-postgres.render.com"
+	dbPort := "5432"
+	dbName := "tectec"
 
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := sql.Open("postgres", dsn)
