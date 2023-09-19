d366f4b51cf1e9739f7a4015759e0ec47ef67b34
Brena Freitas <brena.s.freitas15@gmail.com>
2023-09-18 23:24:44 -0300

    Adicionando função de pesquisar nicknames.

--- static/js/pesquisar.mjs
+++ static/js/pesquisar.mjs
@@ -1,7 +1,5 @@
-var usuariosData = []; // Array para armazenar os dados dos usuários
-
 const token = localStorage.getItem("token").toString();
-
+let usuariosData = []; // Array para armazenar os dados dos usuários
 let idUsuario;
 
 async function varIdUsuario() {
@@ -13,42 +11,50 @@ async function varIdUsuario() {
             },
             body: JSON.stringify({ token: token })
         });
+
         const data = await response.json();
+
         idUsuario = data.usuario.id_usuario;
-        function searchNicknames() {
-            var searchTerm = document.getElementById("searchInput").value;
-            var resultsUl = document.getElementById("results");
-            resultsUl.innerHTML = ""; // Limpa os resultados anteriores
-        
-            // Se os dados já foram carregados, fazer a busca no array local
-            if (usuariosData.length > 0) {
-                for (var i = 0; i < usuariosData.length; i++) {
-                    if (usuariosData[i].nickname === searchTerm) { // Comparação exata, sensível a maiúsculas e minúsculas
-                        var li = document.createElement("li");
-                        li.textContent = usuariosData[i].nickname;
-                        resultsUl.appendChild(li);
-                    }
-                }
-            } else {
-        
-                fetch('/usuarios/' + idUsuario)
-                    .then(response => {
-                        if (!response.ok) {
-                            throw new Error('Erro na solicitação: ' + response.status);
-                        }
-                        return response.json();
-                    })
-                    .then(data => {
-                        usuariosData = data.usuarios; // Armazena os dados no array local
-                        searchNicknames(); // Chama a função de busca novamente
-                    })
-                    .catch(error => {
-                        console.error(error.message);
-                    });
-            }
-        }
+        searchNicknames(); // Chama a função de busca após a conclusão da requisição fetch
     } catch (error) {
         console.error(error);
+    }
+}
+
+
+
+function searchNicknames() {
+    var searchTerm = document.getElementById("searchInput").value;
+    var resultsUl = document.getElementById("results");
+    resultsUl.innerHTML = ""; 
+
+    if (usuariosData.length > 0) {
+        for (var i = 0; i < usuariosData.length; i++) {
+            if (usuariosData[i].nickname === searchTerm) {
+                var li = document.createElement("li");
+                li.textContent = usuariosData[i].nickname;
+                resultsUl.appendChild(li);
+            }
+        }
+    } else {
+        varIdUsuario().then(() => {
+
+            fetch('/usuarios/' + idUsuario)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Erro na solicitação: ' + response.status);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    
+                    usuariosData = data.usuarios; 
+                    searchNicknames(); 
+                })
+                .catch(error => {
+                    console.error(error.message);
+                });
+        });
     }
 }
 
