b285fd581edede9befe027c3f54dbb96683c16b9
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-10-06 22:32:03 -0300

    Organizando os arquivos

--- static/js/header.mjs
+++ static/js/header.mjs
@@ -1,37 +1,39 @@
 // Insira o conteúdo HTML no corpo antes do primeiro filho usando insertAdjacentHTML
 document.body.insertAdjacentHTML('afterbegin', `
+
 <header>
 
 <div class="centraliza">
 
-    <img src="../static/images/casinha.png" alt="" class="user" id="casa" onclick="window.location.href='/home'" title="Home">
+    <img src="../static/images/casinha.png" alt="Home" class="user" id="casa" onclick="window.location.href='/home'" title="Home" style="cursor: pointer;">
 
     <br>
 
-    <img src="../static/images/la_user.png" alt="" class="user" id="perfil" onclick="window.location.href='/perfil'" title="Perfil">
+    <img src="../static/images/la_user.png" alt="Perfil" class="user" id="perfil" onclick="window.location.href='/perfil'" title="Perfil" style="cursor: pointer;">
 
     <br>
 
-    <img src="../static/images/explorar.png" alt="" class="user" id="pesquisar" onclick="window.location.href='/pesquisar'" title="Pesquisar usuário">
+    <img src="../static/images/explorar.png" alt="Pesquisar usuário" class="user" id="pesquisar" onclick="window.location.href='/pesquisar'" title="Pesquisar usuário" style="cursor: pointer;">
 
     <br>
 
-    <img src="../static/images/logo.png" alt="" class="img_logo" id="logo" >
+    <img src="../static/images/logo.png" alt="Logo" class="img_logo" id="logo">
 
     <br>
 
-    <img src="../static/images/pesquisa.png" alt="" class="user" id="busca" title="Tecnologias">
+    <img src="../static/images/pesquisa.png" alt="Tecnologias" class="user" id="busca" title="Tecnologias" style="cursor: pointer;">
 
     <br>
 
-    <img src="../static/images/addpost.png" alt="" class="user" id="addpost" onclick="window.location.href='/post'" title="Postar">
+    <img src="../static/images/addpost.png" alt="Postar" class="user" id="addpost" onclick="window.location.href='/post'" title="Postar" style="cursor: pointer;">
 
     <br>
 
-    <img src="../static/images/out.png" alt="" class="user" id="sair" onclick="window.location.href='/'" title="Sair">
+    <img src="../static/images/out.png" alt="Sair" class="user" id="sair" onclick="window.location.href='/'" title="Sair" style="cursor: pointer;">
 
 </div>
 
 </header>
+
 `);
 
