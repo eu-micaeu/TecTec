20852939d4a0fb6d1dd853d107f992ab7ada22ca
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-10-09 19:13:55 -0300

    Adicionando a extensão.

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Execute o arquivo main em segundo plano
-./main &
+./main.exe &
 
 # Execute o arquivo bot em segundo plano
-./bot &
+./bot.exe &
 
 # Mantenha o script em execução para manter ambos os processos ativos
 tail -f /dev/null
