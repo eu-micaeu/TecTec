978219a6b66b09dc1b588f1a9f4c3b7f035e18f5
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-10-25 22:17:24 -0300

    Arrumando o header.

--- static/js/header.mjs
+++ static/js/header.mjs
@@ -5,18 +5,18 @@ document.body.insertAdjacentHTML('afterbegin', `
 
     <div class="centraliza">
 
-        <img src="../static/images/logo.png" alt="Logo" id="logo" width="45" height="45">
+        <img src="../static/images/logo.png" alt="Logo" id="logo" width="40" height="40">
 
-        <img src="../static/images/casinha.png" alt="Home" id="casa" class="opcao" onclick="window.location.href='/home'" title="Home" width="30" height="30">
+        <img src="../static/images/casinha.png" alt="Home" id="casa" class="opcao" onclick="window.location.href='/home'" title="Home" width="35" height="35">
     
-        <img src="../static/images/la_user.png" alt="Perfil" id="perfil" class="opcao" onclick="window.location.href='/perfil'" title="Perfil" width="30" height="30">
+        <img src="../static/images/la_user.png" alt="Perfil" id="perfil" class="opcao" onclick="window.location.href='/perfil'" title="Perfil" width="35" height="35">
     
-        <img src="../static/images/explorar.png" alt="Pesquisar usuário" id="pesquisar" class="opcao" onclick="window.location.href='/pesquisar'" title="Buscar usuários" width="30" height="30">
+        <img src="../static/images/explorar.png" alt="Pesquisar usuário" id="pesquisar" class="opcao" onclick="window.location.href='/pesquisar'" title="Buscar usuários" width="35" height="35">
 
-        <img src="../static/images/tech.png" alt="Tecnologias" id="busca" class="opcao" title="Tecnologias" width="30" height="30">
+        <img src="../static/images/tech.png" alt="Tecnologias" id="busca" class="opcao" title="Tecnologias" width="35" height="35">
 
-        <img src="../static/images/out.png" alt="Sair" id="sair" class="opcao" title="Sair" width="30" height="30">
-    
+        <img src="../static/images/out.png" alt="Sair" id="sair" class="opcao" title="Sair" width="35" height="35">
+
     </div>
 
 </header>
